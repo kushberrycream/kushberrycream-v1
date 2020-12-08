50019bb8eed7e14185830340e474fd352927ea00
kushberrycream <tom@wilson-express.co.uk>
2020-12-08 22:51:45 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -16,6 +16,11 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': (
+            'pk_test_51HwESEAJAlwUSz6IAqrV2KsAlqJUGC3yy5YVXai783VRGHYs79noGJn\
+SYNTDCpr1Ts4zP3O34jZh0kMaYX1LsMcW00LIWM4TvD'
+        ),
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
