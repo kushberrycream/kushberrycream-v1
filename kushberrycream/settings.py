9f0625c78c1817c48904878fab9e53226705e9fc
kushberrycream <tom@wilson-express.co.uk>
2020-12-17 00:58:12 +0000

    removed secret key and set debug

--- kushberrycream/settings.py
+++ kushberrycream/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@zrbr7_2y+lnd6+6ga28bx!pb22znt*k44tmvn9$b*)_30666w'
+SECRET_KEY = os.getenv('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['kushberrycream-headshop.herokuapp.com', 'localhost']
 
