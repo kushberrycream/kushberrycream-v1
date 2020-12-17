61c13f7827679f031719e65c4356b1719165fdde
kushberrycream <tom@wilson-express.co.uk>
2020-12-17 01:45:13 +0000

    fixed typo on AWS STORAGE BUCKET NAME

--- kushberrycream/settings.py
+++ kushberrycream/settings.py
@@ -184,7 +184,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
     # Bucket Config
-    AWS_STORAFE_BUCKET_NAME = 'kushberrycream-headshop'
+    AWS_STORAGE_BUCKET_NAME = 'kushberrycream-headshop'
     AWS_S3_REGION_NAME = 'eu-west-2'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
